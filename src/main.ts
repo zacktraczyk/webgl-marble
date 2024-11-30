7e93ece03cd2d56b3acdb6480eacd6a96b632d9a
Zack Traczyk <me@zacktraczyk.com>
2024-11-30 17:44:03 -0600

    Create webglUtils

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
 import { mat4 } from "gl-matrix";
 import "./style.css";
+import {
+  Attribute,
+  AttributeSetters,
+  createAttributeSetters,
+  createUniformSetters,
+  initShaderProgram,
+  setAttributes,
+  setUniforms,
+  Uniform,
+  UniformSetters,
+} from "./utils/util";
+
+type ProgramInfo = {
+  program: WebGLProgram;
+  attributeSetters: AttributeSetters;
+  uniformSetters: UniformSetters;
+};
+
+type DrawObject = {
+  programInfo: ProgramInfo;
+  bufferInfo: {
+    numElements: number;
+    attributes: Record<string, Attribute>;
+  };
+  uniforms: Record<string, Uniform>;
+};
 
 function main() {
+  // Get A WebGL context
+
   const canvas = document.querySelector("#gl-canvas");
   if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
     throw new Error("Failed to get canvas element");
@@ -14,8 +42,7 @@ function main() {
     );
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // Create Program Info
 
   const vsSource = `
     attribute vec4 aVertexPosition;
@@ -39,137 +66,25 @@ function main() {
     throw new Error("Failed to initialize shader program");
   }
 
-  const programInfo = {
+  const attribSetters = createAttributeSetters(gl, shaderProgram);
+  const uniformSetters = createUniformSetters(gl, shaderProgram);
+
+  const programInfo: ProgramInfo = {
     program: shaderProgram,
-    attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-    },
-    uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(
-        shaderProgram,
-        "uProjectionMatrix"
-      ),
-      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-    },
+    attributeSetters: attribSetters,
+    uniformSetters,
   };
 
-  const positionBuffer = initPositionBuffer(gl);
+  // Create Draw Objects
 
-  drawScene(gl, programInfo, positionBuffer);
-}
+  const objectsToDraw: DrawObject[] = [];
 
-function drawScene(
-  gl: WebGLRenderingContext,
-  programInfo: any,
-  positionBuffer: WebGLBuffer
-) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clearDepth(1.0);
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL);
+  // Create Circle
 
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  let num_vertices = 20;
 
-  const fieldOfView = (45 * Math.PI) / 180;
-  if (!(gl.canvas instanceof HTMLCanvasElement)) {
-    throw new Error("Failed to get canvas element");
-  }
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = mat4.create();
-
-  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-
-  const modelViewMatrix = mat4.create();
-
-  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  gl.vertexAttribPointer(
-    programInfo.attribLocations.vertexPosition,
-    2,
-    gl.FLOAT,
-    false,
-    0,
-    0
-  );
-  gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
-
-  gl.useProgram(programInfo.program);
-
-  gl.uniformMatrix4fv(
-    programInfo.uniformLocations.projectionMatrix,
-    false,
-    projectionMatrix
-  );
-  gl.uniformMatrix4fv(
-    programInfo.uniformLocations.modelViewMatrix,
-    false,
-    modelViewMatrix
-  );
-
-  gl.drawArrays(gl.LINE_LOOP, 0, num_vertices);
-}
-
-function initShaderProgram(
-  gl: WebGLRenderingContext,
-  vsSource: string,
-  fsSource: string
-) {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  if (!vertexShader || !fragmentShader) {
-    throw new Error("Failed to load shaders");
-  }
-
-  const shaderProgram = gl.createProgram();
-  if (!shaderProgram) {
-    throw new Error("Failed to create shader program");
-  }
-
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert(
-      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
-        shaderProgram
-      )}`
-    );
-    return null;
-  }
-
-  return shaderProgram;
-}
-
-function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
-  const shader = gl.createShader(type);
-  if (!shader) {
-    throw new Error("Failed to create shader");
-  }
-
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(
-      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
-
-  return shader;
-}
-
-let num_vertices = 20;
-
-function initPositionBuffer(gl: WebGLRenderingContext) {
   const positionBuffer = gl.createBuffer();
+
   if (!positionBuffer) {
     throw new Error("Failed to create buffer");
   }
@@ -189,15 +104,69 @@ function initPositionBuffer(gl: WebGLRenderingContext) {
       center[1] + radius * Math.sin(theta)
     );
   }
-  // for (let a = 0; a < 360; a += 360/sides) {
-  //   const heading = a * Math.PI / 180;
-  //   vertices.add(
-
-  //   }
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  return positionBuffer;
+  const circleObject: DrawObject = {
+    programInfo,
+    bufferInfo: {
+      numElements: num_vertices,
+      attributes: {
+        aVertexPosition: {
+          attributeType: "buffer",
+          buffer: positionBuffer,
+          size: 2,
+          type: gl.FLOAT,
+          normalize: false,
+          stride: 0,
+          offset: 0,
+        },
+      },
+    },
+    uniforms: {
+      uProjectionMatrix: mat4.create(),
+      uModelViewMatrix: mat4.create(),
+    },
+  };
+  objectsToDraw.push(circleObject);
+
+  drawScene(gl, objectsToDraw);
+}
+
+function drawScene(gl: WebGLRenderingContext, objectsToDraw: DrawObject[]) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(1.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  const fieldOfView = (45 * Math.PI) / 180;
+  if (!(gl.canvas instanceof HTMLCanvasElement)) {
+    throw new Error("Failed to get canvas element");
+  }
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+
+  objectsToDraw.forEach((object) => {
+    // TODO: Fix uMatrixs
+    const projectionMatrix = object.uniforms["uProjectionMatrix"] as mat4;
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+    const modelViewMatrix = object.uniforms["uModelViewMatrix"] as mat4;
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+
+    gl.useProgram(object.programInfo.program);
+
+    setAttributes(
+      object.programInfo.attributeSetters,
+      object.bufferInfo.attributes
+    );
+
+    setUniforms(object.programInfo.uniformSetters, object.uniforms);
+
+    gl.drawArrays(gl.LINE_LOOP, 0, object.bufferInfo.numElements);
+  });
 }
 
 main();
---
+++ src/utils/util.ts
@@ -0,0 +1,445 @@
+/*
+ * Source: https://webglfundamentals.org/webgl/resources/webgl-utils.js
+ *
+ * Copyright 2021 GFXFundamentals.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of GFXFundamentals. nor the names of his
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// type BufferInfo = {
+//   attribs: Record<string, Attribute>;
+//   numElements: number;
+
+//   indices: any; // TypedArray
+//   position: any;
+//   normal: any;
+//   texCoords: any;
+// };
+
+// type Attribs = {
+//   buffer: WebGLBuffer;
+//   numComponents: number;
+//   // type: number;
+//   // nomralize: boolean;
+// };
+
+// Value or Buffer
+export type Attribute =
+  | {
+      attributeType: "value";
+
+      value:
+        | [number, number, number, number]
+        | [number, number, number]
+        | [number, number]
+        | [number];
+    }
+  | {
+      attributeType: "buffer";
+
+      buffer: WebGLBuffer;
+      size: number;
+      type?: number;
+      normalize?: boolean;
+      stride?: number;
+      offset?: number;
+    };
+
+export type AttributeSetters = Record<string, (b: Attribute) => void>;
+
+export type Uniform = Float32Array | number[];
+
+export type UniformSetters = Record<string, (v: Uniform) => void>;
+
+/**
+ * Initialize a shader program
+ * @param gl a WebGLRenderingContext
+ * @param vsSource a vertex shader source
+ * @param fsSource a fragment shader source
+ * @returns a WebGLProgram
+ */
+export function initShaderProgram(
+  gl: WebGLRenderingContext,
+  vsSource: string,
+  fsSource: string
+) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  if (!vertexShader || !fragmentShader) {
+    throw new Error("Failed to load shaders");
+  }
+
+  const shaderProgram = gl.createProgram();
+  if (!shaderProgram) {
+    throw new Error("Failed to create shader program");
+  }
+
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert(
+      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+        shaderProgram
+      )}`
+    );
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+/**
+ * Load a shader
+ * @param gl a WebGLRenderingContext
+ * @param type a type of shader
+ * @param source a shader source
+ * @returns a WebGLShader
+ */
+function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
+  const shader = gl.createShader(type);
+  if (!shader) {
+    throw new Error("Failed to create shader");
+  }
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(
+      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`
+    );
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+/**
+ * Create a set of attribute setters for a program
+ * @param gl a WebGLRenderingContext
+ * @param program a WebGLProgram
+ */
+export function createAttributeSetters(
+  gl: WebGLRenderingContext,
+  program: WebGLProgram
+): AttributeSetters {
+  /**
+   * Create a setter for an attribute
+   * @param index index of the attribute
+   * @returns a function that sets the attribute
+   */
+  const createAttribSetter = (index: number) => {
+    return (b: Attribute) => {
+      if (b.attributeType === "value") {
+        gl.disableVertexAttribArray(index);
+        switch (b.value.length) {
+          case 4:
+            gl.vertexAttrib4fv(index, b.value);
+            break;
+          case 3:
+            gl.vertexAttrib3fv(index, b.value);
+            break;
+          case 2:
+            gl.vertexAttrib2fv(index, b.value);
+            break;
+          case 1:
+            gl.vertexAttrib1fv(index, b.value);
+            break;
+          default:
+            throw new Error("the length of value must be between 1 and 4");
+        }
+      } else {
+        gl.bindBuffer(gl.ARRAY_BUFFER, b.buffer);
+        gl.enableVertexAttribArray(index);
+        gl.vertexAttribPointer(
+          index,
+          b.size,
+          b.type || gl.FLOAT,
+          b.normalize || false,
+          b.stride || 0,
+          b.offset || 0
+        );
+      }
+    };
+  };
+
+  const attribSetters: Record<string, (b: Attribute) => void> = {};
+
+  const numAttribs = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+  for (let i = 0; i < numAttribs; i++) {
+    const attribInfo = gl.getActiveAttrib(program, i);
+    if (!attribInfo) {
+      break;
+    }
+    const index = gl.getAttribLocation(program, attribInfo.name);
+    attribSetters[attribInfo.name] = createAttribSetter(index);
+  }
+
+  return attribSetters;
+}
+
+/**
+ * Get the bind point for a given sampler type
+ * @param gl a WebGLRenderingContext
+ * @param type a type of sampler
+ * @returns a bind point for a given sampler type
+ */
+function getBindPointForSamplerType(gl: WebGLRenderingContext, type: number) {
+  if (type === gl.SAMPLER_2D) return gl.TEXTURE_2D;
+  if (type === gl.SAMPLER_CUBE) return gl.TEXTURE_CUBE_MAP;
+  return undefined;
+}
+
+// TODO: Verify the type of UniformInfo
+type UniformInfo = {
+  name: string;
+  size: number;
+  type: number;
+};
+
+/**
+ * Create a set of uniform setters for a program
+ * @param gl a WebGLRenderingContext
+ * @param program a WebGLProgram
+ */
+export function createUniformSetters(
+  gl: WebGLRenderingContext,
+  program: WebGLProgram
+): UniformSetters {
+  let textureUnit = 0;
+
+  const createUniformSetter = (
+    program: WebGLProgram,
+    uniformInfo: UniformInfo
+  ) => {
+    const location = gl.getUniformLocation(program, uniformInfo.name);
+    const type = uniformInfo.type;
+    const isArray =
+      uniformInfo.size > 1 && uniformInfo.name.slice(-3) === "[0]";
+
+    if (type === gl.FLOAT && isArray) {
+      return (v: [GLfloat]) => {
+        gl.uniform1fv(location, v);
+      };
+    }
+    if (type === gl.FLOAT) {
+      return (v: GLfloat) => {
+        gl.uniform1f(location, v);
+      };
+    }
+    if (type === gl.FLOAT_VEC2) {
+      return (v: [GLfloat, GLfloat]) => {
+        gl.uniform2fv(location, v);
+      };
+    }
+    if (type === gl.FLOAT_VEC3) {
+      return (v: [GLfloat, GLfloat, GLfloat]) => {
+        gl.uniform3fv(location, v);
+      };
+    }
+    if (type === gl.FLOAT_VEC4) {
+      return (v: [GLfloat, GLfloat, GLfloat, GLfloat]) => {
+        gl.uniform4fv(location, v);
+      };
+    }
+    if (type === gl.INT && isArray) {
+      return (v: [GLint]) => {
+        gl.uniform1iv(location, v);
+      };
+    }
+    if (type === gl.INT) {
+      return (v: GLint) => {
+        gl.uniform1i(location, v);
+      };
+    }
+    if (type === gl.INT_VEC2) {
+      return (v: [GLint, GLint]) => {
+        gl.uniform2iv(location, v);
+      };
+    }
+    if (type === gl.INT_VEC3) {
+      return (v: [GLint, GLint, GLint]) => {
+        gl.uniform3iv(location, v);
+      };
+    }
+    if (type === gl.INT_VEC4) {
+      return (v: [GLint, GLint, GLint, GLint]) => {
+        gl.uniform4iv(location, v);
+      };
+    }
+    if (type === gl.BOOL) {
+      return (v: [GLint]) => {
+        gl.uniform1iv(location, v);
+      };
+    }
+    if (type === gl.BOOL_VEC2) {
+      return (v: [GLint, GLint]) => {
+        gl.uniform2iv(location, v);
+      };
+    }
+    if (type === gl.BOOL_VEC3) {
+      return (v: [GLint, GLint, GLint]) => {
+        gl.uniform3iv(location, v);
+      };
+    }
+    if (type === gl.BOOL_VEC4) {
+      return (v: [GLint, GLint, GLint, GLint]) => {
+        gl.uniform4iv(location, v);
+      };
+    }
+    if (type === gl.FLOAT_MAT2) {
+      return (v: [GLfloat, GLfloat, GLfloat, GLfloat]) => {
+        gl.uniformMatrix2fv(location, false, v);
+      };
+    }
+    if (type === gl.FLOAT_MAT3) {
+      return (
+        v: [
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat
+        ]
+      ) => {
+        gl.uniformMatrix3fv(location, false, v);
+      };
+    }
+    if (type === gl.FLOAT_MAT4) {
+      return (
+        v: [
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat,
+          GLfloat
+        ]
+      ) => {
+        gl.uniformMatrix4fv(location, false, v);
+      };
+    }
+
+    if ((type === gl.SAMPLER_2D || type === gl.SAMPLER_CUBE) && isArray) {
+      const units = [];
+      for (let ii = 0; ii < uniformInfo.size; ++ii) {
+        units.push(textureUnit++);
+      }
+      return (function (bindPoint: GLenum, units) {
+        return function (textures: WebGLTexture[]) {
+          gl.uniform1iv(location, units);
+          textures.forEach(function (texture, index) {
+            gl.activeTexture(gl.TEXTURE0 + units[index]);
+            gl.bindTexture(bindPoint, texture);
+          });
+        };
+      })(getBindPointForSamplerType(gl, type)!, units);
+    }
+    if (type === gl.SAMPLER_2D || type === gl.SAMPLER_CUBE) {
+      return (function (bindPoint: GLenum, unit) {
+        return function (texture: WebGLTexture) {
+          gl.uniform1i(location, unit);
+          gl.activeTexture(gl.TEXTURE0 + unit);
+          gl.bindTexture(bindPoint, texture);
+        };
+      })(getBindPointForSamplerType(gl, type)!, textureUnit++);
+    }
+    throw "unknown type: 0x" + type.toString(16); // we should never get here.
+  };
+
+  const uniformSetters: Record<string, (v: any) => void> = {};
+  const numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+
+  for (let i = 0; i < numUniforms; i++) {
+    const uniformInfo = gl.getActiveUniform(program, i);
+    if (!uniformInfo) {
+      break;
+    }
+    let name = uniformInfo.name;
+
+    // remove the array suffix.
+    if (name.slice(-3) === "[0]") {
+      name = name.slice(0, name.length - 3);
+    }
+    const setter = createUniformSetter(program, uniformInfo);
+    uniformSetters[name] = setter;
+  }
+
+  return uniformSetters;
+}
+
+/**
+ * Set Attributes for a program
+ * @param setters a set of attribute setters
+ * @param attributes a set of attributes
+ */
+export function setAttributes(
+  setters: AttributeSetters,
+  attributes: Record<string, Attribute>
+) {
+  Object.keys(attributes).forEach((name) => {
+    const setter = setters[name];
+    if (setter) {
+      setter(attributes[name]);
+    }
+  });
+}
+
+/**
+ * Set Uniforms for a program
+ * @param setters a set of uniform setters
+ * @param uniforms a set of uniforms
+ */
+export function setUniforms(
+  setters: UniformSetters,
+  uniforms: Record<string, Uniform>
+) {
+  Object.keys(uniforms).forEach((name) => {
+    const setter = setters[name];
+    if (setter) {
+      setter(uniforms[name]);
+    }
+  });
+}
