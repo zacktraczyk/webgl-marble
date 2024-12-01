72ad784672b1660240285479f7cc8768ba355b96
Zack Traczyk <zack@tribexr.com>
2024-12-01 01:18:28 -0600

    Simplified shader to 2d and animated

--- src/glsl/vert.glsl
+++ src/glsl/vert.glsl
@@ -1,8 +1,20 @@
-attribute vec4 aVertexPosition;
+attribute vec2 aVertexPosition;
 
-uniform mat4 uModelViewMatrix;
-uniform mat4 uProjectionMatrix;
+uniform vec2 uResolution;
+uniform vec2 uTranslation;
 
 void main(void) {
-  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  // Add in the translation
+  vec2 position = aVertexPosition + uTranslation;
+
+  // convert the position from pixels to 0.0 to 1.0
+  vec2 zeroToOne = position / uResolution;
+
+  // convert from 0->1 to 0->2
+  vec2 zeroToTwo = zeroToOne * 2.0;
+
+  // convert from 0->2 to -1->+1 (clipspace)
+  vec2 clipSpace = zeroToTwo - 1.0;
+
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
