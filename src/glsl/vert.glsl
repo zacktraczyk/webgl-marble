5c2a6075084bb4d66ac37f05847641ca1b7a0bc8
Zack Traczyk <zack@tribexr.com>
2024-11-30 19:44:11 -0600

    Extract glsl to files

---
+++ src/glsl/vert.glsl
@@ -0,0 +1,8 @@
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main(void) {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
