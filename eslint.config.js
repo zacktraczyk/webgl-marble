661355e4f0dd65ba8db06ae2f901a5939f62fb94
Zack Traczyk <me@zacktraczyk.com>
2024-12-08 14:23:59 -0800

    Add tailwindcss

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import tailwind from "eslint-plugin-tailwindcss";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  pluginJs.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  tseslint.configs.eslintRecommended,
+  ...tailwind.configs["flat/recommended"],
+  prettier,
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
     },
