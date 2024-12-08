7a39438a8c2a6d9dd19aaa2e80d4a10684006887
Zack Traczyk <zack@tribexr.com>
2024-12-08 13:13:47 -0800

    More circles and squares

--- src/main.ts
+++ src/main.ts
@@ -6,35 +6,37 @@ import { VDU } from "./utils/vdu";
 function main() {
   const vdu = new VDU("#gl-canvas");
 
-  const c1 = new Circle({
-    position: [100, 100],
-    radius: 50,
-  });
-  vdu.add(c1);
+  const circles: Circle[] = [];
+  for (let i = 0; i < 10; i++) {
+    const c = new Circle({
+      position: [50 + i * 10, 50 + i * 10],
+      radius: 20,
+    });
+    vdu.add(c);
+    circles.push(c);
+  }
 
-  const c2 = new Circle({
-    position: [150, 150],
-    radius: 50,
-  });
-  vdu.add(c2);
-
-  const s1 = new Square({
-    position: [50, 50],
-    width: 50,
-  });
-  vdu.add(s1);
+  const squares: Square[] = [];
+  for (let i = 0; i < 10; i++) {
+    const s = new Square({
+      position: [240, 50 + i * 10],
+      width: 20,
+    });
+    vdu.add(s);
+    squares.push(s);
+  }
 
   function updateScene(time: number) {
     time *= 0.005;
 
-    c1.position[0] += Math.cos(time) * 2;
-    c1.position[1] += Math.sin(time / 2) * 2;
+    circles.forEach((c, i) => {
+      c.position[0] += Math.sin(time + i);
+      c.position[1] += Math.cos(time + i);
+    });
 
-    c2.position[0] += Math.sin(time);
-    c2.position[1] += Math.cos(time);
-
-    s1.position[0] += Math.sin(time) * 5;
-    s1.position[1] += Math.sin(time) * 5;
+    squares.forEach((s, i) => {
+      s.position[0] += Math.cos(time + i);
+    });
   }
 
   function render(time: number) {
