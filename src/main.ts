476cb4ee6877ea2a1fd8af7e8da3a5ee913c49d0
Zack Traczyk <me@zacktraczyk.com>
2024-12-08 15:58:18 -0800

    Add color

--- src/main.ts
+++ src/main.ts
@@ -27,28 +27,16 @@ function main() {
     squares.push(s);
   }
 
-  let lastTime = 0;
-  const fpsElem = document.getElementById("#fps");
-  const [updateFpsText] = throttle((fps: number) => {
-    if (fpsElem) {
-      fpsElem.textContent = `FPS: ${fps.toFixed(2)}`;
-    }
-  }, 500);
-
-  const updateFps = (time: number) => {
-    time *= 0.005;
-    const deltaTime = time - lastTime;
-    lastTime = time;
-    const fps = 1 / deltaTime;
-    updateFpsText(fps);
-  };
-
   let tick = 0;
   function updateScene(time: number) {
     tick += 0.01;
     updateFps(time);
 
     circles.forEach((c, i) => {
+      c.color[0] = Math.sin(tick + i + Math.PI / 2) * 0.25 + 0.5;
+      c.color[1] = Math.sin(tick * 3 + i + Math.PI / 3) * 0.25 + 0.5;
+      c.color[2] = Math.sin(tick * 4 + i + Math.PI) * 0.25 + 0.5;
+
       c.position[0] += Math.sin(tick + i);
       c.position[1] += Math.cos(tick + i);
     });
@@ -67,5 +55,22 @@ function main() {
 
   requestAnimationFrame(render);
 }
+
+// FPS Counter
+let lastTime = 0;
+const fpsElem = document.getElementById("#fps");
+const [updateFpsText] = throttle((fps: number) => {
+  if (fpsElem) {
+    fpsElem.textContent = `FPS: ${fps.toFixed(2)}`;
+  }
+}, 500);
+
+const updateFps = (time: number) => {
+  time *= 0.005;
+  const deltaTime = time - lastTime;
+  lastTime = time;
+  const fps = 1 / deltaTime;
+  updateFpsText(fps);
+};
 
 main();
