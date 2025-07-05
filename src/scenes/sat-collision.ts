f6d4251edf9ee777a3b86cbc602d4383be76882b
Zack Traczyk <me@zacktraczyk.com>
2025-07-05 10:05:25 -0700

    Cleanup SAT collision scene

--- src/scenes/sat-collision.ts
+++ src/scenes/sat-collision.ts
@@ -1,9 +1,7 @@
-import { Line } from "../engine/object/line";
 import { PhysicsEntity, type Physical } from "../engine/physics/entitySAT";
 import Physics from "../engine/physics/physicsSAT";
 import Stage from "../engine/stage";
 import { type DragAndDroppable } from "../engine/stage/eventHandlers";
-import { getNext } from "../engine/utils/id";
 import {
   createCircle,
   createHexagon,
@@ -36,6 +34,16 @@ function main() {
   stage.add(circle1);
   physicsSAT.add(circle1);
 
+  const circle2 = new DragAndDropCircle({
+    position: [centerX, centerY + offset],
+    radius: 70,
+    color: [167 / 255, 139 / 255, 250 / 255, 1],
+    handleRadius: 15,
+    handleColor: [0.4, 0.4, 0.4, 1],
+  });
+  stage.add(circle2);
+  physicsSAT.add(circle2);
+
   const square1 = new DragAndDropRectangle({
     position: [centerX - offset, centerY],
     width: 100,
@@ -59,103 +67,6 @@ function main() {
   });
   stage.add(hexagon1);
   physicsSAT.add(hexagon1);
-
-  let debugSlope: Line | null = null;
-  let debugEdge: Line | null = null;
-
-  let debugAxis: Line | null = null;
-  let debugAxisProj1: Line | null = null;
-  let debugAxisProj2: Line | null = null;
-  physicsSAT.register(({ type, ...data }) => {
-    if (type !== "debug-sat" || !("data" in data)) {
-      return;
-    }
-
-    const { slope, edge, proj1, proj2 } = data.data;
-    if (!debugSlope) {
-      // Draw a line through the center of the canvas with slope
-      const centerX = stage.canvas.clientWidth / 2;
-      const centerY = stage.canvas.clientHeight / 2;
-
-      const startX = centerX - 100;
-      const startY = slope * (startX - centerX) + centerY;
-
-      const endX = centerX + 100;
-      const endY = slope * (endX - centerX) + centerY;
-
-      const lineDrawEntity = new Line({
-        startPosition: [startX, startY],
-        endPosition: [endX, endY],
-        stroke: 2,
-        color: [1, 1, 1, 1],
-      });
-      stage.add(lineDrawEntity);
-      debugSlope = lineDrawEntity;
-    }
-
-    if (!debugAxis) {
-      const posY = centerY + 300;
-      const axisLength = 300;
-
-      const startPosition: [number, number] = [centerX - axisLength / 2, posY];
-      const endPosition: [number, number] = [centerX + axisLength / 2, posY];
-
-      const lineDrawEntity = new Line({
-        startPosition: startPosition,
-        endPosition: endPosition,
-        stroke: 2,
-        color: [1, 1, 1, 1],
-      });
-      stage.add(lineDrawEntity);
-      debugAxis = lineDrawEntity;
-
-      const proj1OffsetY = posY - 50;
-      const proj1StartPosition: [number, number] = [
-        startPosition[0] + proj1[0] * axisLength,
-        proj1OffsetY,
-      ];
-      const proj1EndPosition: [number, number] = [
-        startPosition[0] + proj1[1] * axisLength,
-        proj1OffsetY,
-      ];
-      const proj1LineDrawEntity = new Line({
-        startPosition: proj1StartPosition,
-        endPosition: proj1EndPosition,
-        stroke: 2,
-        color: [0, 0.2, 0.2, 1],
-      });
-      stage.add(proj1LineDrawEntity);
-
-      const proj2OffsetY = posY - 100;
-      const proj2StartPosition: [number, number] = [
-        startPosition[0] + proj2[0] * axisLength,
-        proj2OffsetY,
-      ];
-      const proj2EndPosition: [number, number] = [
-        startPosition[0] + proj2[1] * axisLength,
-        proj2OffsetY,
-      ];
-
-      const proj2LineDrawEntity = new Line({
-        startPosition: proj2StartPosition,
-        endPosition: proj2EndPosition,
-        stroke: 2,
-        color: [0, 0.2, 0.2, 1],
-      });
-      stage.add(proj2LineDrawEntity);
-    }
-
-    if (!debugEdge) {
-      const lineDrawEntity = new Line({
-        startPosition: edge[0],
-        endPosition: edge[1],
-        stroke: 2,
-        color: [187 / 255, 27 / 255, 219 / 255, 0.8],
-      });
-      stage.add(lineDrawEntity);
-      debugEdge = lineDrawEntity;
-    }
-  });
 
   let collisions: [string, string][] = [];
   physicsSAT.register(({ type, ...data }) => {
